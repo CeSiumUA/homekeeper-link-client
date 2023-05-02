f5b2ebbdcdc94f83437a55ebaa41dc0dba6e8c5b
Fedir Katushonok <46378315+CeSiumUA@users.noreply.github.com>
2023-05-02 07:36:27 +0300

    Added mqtt and IP address fetching

--- src/main.py
+++ src/main.py
@@ -7,8 +7,13 @@ import logging
 from apscheduler.schedulers.blocking import BlockingScheduler
 from nacl.signing import SigningKey
 from nacl.encoding import HexEncoder
+from paho.mqtt import client as mqtt_client
+import random
+import topics
 
 key_file = 'keys/private_key'
+KEEP_ALIVE_REQUEST = 0
+IP_ADDRESS_REQUEST = 1
 
 def form_message():
     key = None
@@ -21,7 +26,8 @@ def form_message():
 
     msg = {
         "client_id": key["client_id"],
-        "signature": signed_msg
+        "signature": signed_msg,
+        "request_type": KEEP_ALIVE_REQUEST
     }
 
     return msgpack.packb(msg)
@@ -33,6 +39,35 @@ def ping_server():
         bs = client.send(msg)
         logging.info("{} bytes sent to server".format(bs))
 
+def on_mqtt_connect(client, userdata, flags, rc):
+    if rc == 0:
+        logging.info("Connected to MQTT")
+    else:
+        logging.fatal("Failed to connect to MQTT, return code: %d\n", rc)
+
+def on_mqtt_message(client: mqtt_client.Client, userdata, msg):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sck:
+        sck.connect((server_address, server_port))
+        msg = {
+            'request_type': IP_ADDRESS_REQUEST
+        }
+
+        bs = sck.send(msgpack.packb(msg))
+        logging.info("%d bytes sent to server", bs)
+        result = sck.recv(1024)
+        result = msgpack.unpackb(result)
+        result_addr = result['ip_address']
+        client.publish(topic=topics.SEND_MESSAGE, payload=f"homeserver ip address: {result_addr}")
+
+def start_mqtt(broker: str, port: int = 1883):
+    client_id = "link-client-{}".format(random.randint(0, 1000))
+    client = mqtt_client.Client(client_id=client_id)
+    client.on_connect = on_mqtt_connect
+    client.connect(broker, port=port)
+    client.loop_start()
+
+    client.subscribe(topic=topics.GET_IP_ADDRESS)
+    client.on_message = on_mqtt_message
 
 if __name__ == '__main__':
     load_dotenv()
@@ -42,6 +77,18 @@ if __name__ == '__main__':
     server_address = environ.get("SERVER_ADDR")
     server_port = environ.get("SERVER_PORT")
     sending_interval = environ.get("SENDING_INTERVAL")
+
+    broker_host = environ.get("MQTT_HOST")
+    if broker_host is None:
+        logging.fatal("could not load mqtt host")
+    else:
+        logging.info("MQTT host: %s", broker_host)
+    broker_port = environ.get("MQTT_PORT")
+    if broker_port is None:
+        broker_port = 1883
+    else:
+        broker_port = int(broker_port)
+    logging.info("MQTT port: %d", broker_port)
 
     if not isfile(key_file):
         print("standart location is empty")
@@ -63,6 +110,8 @@ if __name__ == '__main__':
 
     scheduler = BlockingScheduler()
     scheduler.add_job(ping_server, 'interval', seconds=sending_interval)
+
+    start_mqtt(broker_host, broker_port)
 
     try:
         logging.info("starting scheduler...")
